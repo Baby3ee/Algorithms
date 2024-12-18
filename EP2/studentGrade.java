e55e835264d7f1a14f88fa6e92a7570582952ed3
Rawiwan <beerawiwan1626@gmail.com>
2024-12-18 09:18:20 +0700

    Update studentGrade.java

---
+++ EP2/studentGrade.java
@@ -0,0 +1,7 @@
+package EP2;
+import java.util.Scanner;
+public class studentGrade {
+    public static void main(String[] args) {}
+    int score[] = new int[10];
+    Scanner ScanIn = new Scanner(System.in);
+}
