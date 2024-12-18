e6ab12f9e4f3048bd8524d708c261da452214ec0
Rawiwan <beerawiwan1626@gmail.com>
2024-12-18 11:50:30 +0700

    Update loop.java

--- Qiuzz1/loop.java
+++ Qiuzz1/loop.java
@@ -5,11 +5,17 @@ public class loop {
     Scanner ScanIn = new Scanner(System.in);
     System.out.print("Enter N :");
         int n = ScanIn.nextInt();
-        for(int i=1; i<= n; i++){ // *จะทำการเริ่มตั้งแต่1ถึง5 โดยi++เป็นการเพิ่มค่าทีละ1
+        for(int i=n; i<= n; i++){ // *จะทำการเริ่มตั้งแต่1ถึง5 โดยi++เป็นการเพิ่มค่าทีละ1
             for(int j = 1; j<=i; j++){
                 System.out.print("*");
             }
             System.out.println();
         }
+        for(int i = n; i>= n; i--){ //จะเป็นการไล่จาก4ถึง1 โดยi--เป็นการลดค่าทีละ1
+            for(int j = 1; j<= i; j++){
+                System.out.print("*");
+            }
+            System.out.println();
     }
+}
 }
